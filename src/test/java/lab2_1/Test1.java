8d36dc59c3d6290dbe20396492195fe7365914ee
AliakseiDatskevich <187672@edu.p.lodz.pl>
2017-12-16 16:50:53 +0100

    testy_v1. Added tests

--- src/test/java/lab2_1/Test1.java
+++ src/test/java/lab2_1/Test1.java
@@ -8,11 +8,74 @@ import edu.iis.mto.bsearch.SearchResult;
 public class Test1 {
 
     @Test
-    public void test() {
-        int seq[] = {1, 3, 4, 5, 6, 8, 13, 7};
+    public void search() {
+        System.out.println("=============");
+        System.out.println("Search test:");
+        int key = 8;
+        int seq[] = {1, 3, 4, 5, 6, 8, 13, 21};
         new BinarySearch();
-        SearchResult searchRes = BinarySearch.search(13, seq);
+        SearchResult searchRes = BinarySearch.search(key, seq);
         System.out.println(searchRes.getPosition());
+        System.out.println("=============\n");
     }
 
+    @Test
+    public void noKeyInSeq() {
+        System.out.println("=============");
+        System.out.println("No key in sequence test:");
+        int key = 8;
+        int seq[] = {1, 2, 6};
+        new BinarySearch();
+        SearchResult searchRes = BinarySearch.search(key, seq);
+        System.out.println(searchRes.getPosition());
+        System.out.println("=============\n");
+    }
+
+    @Test
+    public void firstKeyPosition() {
+        System.out.println("=============");
+        System.out.println("First key position test:");
+        int key = 8;
+        int seq[] = {8, 9, 14, 65, 99};
+        new BinarySearch();
+        SearchResult searchRes = BinarySearch.search(key, seq);
+        System.out.println(searchRes.getPosition());
+        System.out.println("=============\n");
+    }
+
+    @Test
+    public void lastKeyPosition() {
+        System.out.println("=============");
+        System.out.println("Last key position test:");
+        int key = 8;
+        int seq[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        new BinarySearch();
+        SearchResult searchRes = BinarySearch.search(key, seq);
+        System.out.println(searchRes.getPosition());
+        System.out.println("=============\n");
+    }
+
+    @Test
+    public void midleKeyPosition() {
+        System.out.println("=============");
+        System.out.println("Midle key position test:");
+        int key = 7;
+        int seq[] = {1, 2, 3, 4, 5, 6, 7};
+        new BinarySearch();
+        SearchResult searchRes = BinarySearch.search(key, seq);
+        System.out.println(searchRes.getPosition());
+        System.out.println("=============\n");
+    }
+
+    @Test
+    public void emptySeq() {
+        System.out.println("=============");
+        System.out.println("Empty sequence test:");
+        int key = 7;
+        int seq[] = {};
+        new BinarySearch();
+        SearchResult searchRes = BinarySearch.search(key, seq);
+        System.out.println(searchRes.getPosition());
+        System.out.println("=============\n");
+    }
 }
